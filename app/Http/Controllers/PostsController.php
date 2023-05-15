7ee6cec79e99f227403161d0d1a4bf375d71bde0
清水恵里 <a250159a@gamil.com>
2023-05-15 20:36:53 +0900

    投稿更新機能の実装

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,6 +14,7 @@ class PostsController extends Controller
         $id = Auth::id();
         $lists = \DB::table('users')
         ->select('posts.*','users.username','users.images')
+        ->select('posts.user_id','posts.id as post_id','posts.posts','posts.created_at','posts.updated_at','users.id','users.username','users.images')
         ->join('posts','users.id','posts.user_id')
         ->leftJoin('follows','users.id','=','follows.follow')
         ->where('posts.user_id',$id)
@@ -33,5 +34,19 @@ class PostsController extends Controller
             'created_at' => \Carbon\Carbon::now()->toDateTimeString()
         ]);
         return redirect('top'); //view('posts.index')ではエラーが出る
+    }
+
+    public function postUpdate(Request $request){
+        $post = $request->input('updatePost');
+        $post_id = $request->input('post_id');
+        $id = Auth::id();
+        DB::table('posts')
+            ->where('id',$post_id)
+            ->update([
+                'posts' => $post,
+                'user_id' => $id,
+                'updated_at' => \Carbon\Carbon::now()->toDateTimeString()
+            ]);
+        return redirect('/top');
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,16 +3,33 @@
 @section('content')
 <h2>機能を実装していきましょう。</h2>
 {{ Form::open(['url' => 'create']) }}
-<img src="/images/icons/{{ Auth::user()->images }}">
-{{ Form::textarea('newPost',null,['class' => 'post-form','placeholder' => '何をつぶやこうか...?']) }}
+<img src="/storage/{{ Auth::user()->images }}">
+{{ Form::textarea('newPost',null,['class' => 'post-form','placeholder' => '何をつぶやこうか...?','maxlength'=>'150']) }}
 {{ Form::input('hidden', 'user_id', '$user->id') }}
 {{ Form::submit() }}
 {{ Form::close() }}
 @foreach ($lists as $lists)
-<div><img src="/images/icons/{{ $lists->images }}"></div>
-<div>{{ $lists->username }}</div>
-<div>{{ $lists->posts }}</div>
-<div>{{ $lists->created_at }}</div>
+  @if($lists->id === Auth::user()->id)
+    <div><a href="/profile"><img src="/storage/{{ $lists->images }}"></a></div>
+    <div>{{ $lists->username }}</div>
+    <div>{{ $lists->posts }}</div>
+    <div>{{ $lists->created_at }}</div>
+    <div class="modalOpen"><img src="/images/edit.png"></div>
+    <div class="updateForm-wrap">
+
+        {{ Form::open(['url' => '/postUpdate']) }}
+        {{ Form::textarea('updatePost',null,['class'=>'updateForm','placeholder'=>$lists->posts,'maxlength'=>'150']) }}
+        {{ Form::hidden('post_id', $lists->post_id) }}
+        {{ Form::button('<img src="/images/edit.png">',['type' => 'submit']) }}
+        {{ Form::close() }}
+    </div>
+    <div><a href="/postDelete"><img src="/images/trash.png"></a></div>
+  @else
+    <div><a href="/user{{ $lists->id }}"><img src="/storage/{{ $lists->images }}"></a></div>
+    <div>{{ $lists->username }}</div>
+    <div>{{ $lists->posts }}</div>
+    <div>{{ $lists->created_at }}</div>
+    @endif
 @endforeach
 
 
